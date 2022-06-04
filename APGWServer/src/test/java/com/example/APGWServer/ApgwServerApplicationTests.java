48780b9ea2825e7ce183aa646933da114d7cd053
Sindy Quispe Pozo <sindyquispe380@gmail.com>
2022-06-03 23:56:16 -0500

    APGW

---
+++ APGWServer/src/test/java/com/example/APGWServer/ApgwServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.APGWServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApgwServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
